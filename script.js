e0136bf793f98a6da14c3bd8b072fee04efb9de8
Simone Sgarlata <sgarlatasimone@gmail.com>
2023-09-26 21:06:46 +0200

    aggiunta logica per evitare lettere maiuscole nell'input, fix grafica

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let description = document.createElement("div"); // creo l'elemento description per visualizzare il div con la descrizione
-description.textContent = "Start searching a new city and view the results"; // assegno un valore al description
+description.textContent =
+  "Start searching a new city and view the results (only in english)"; // assegno un valore al description
 document.body.appendChild(description); // aggiungo l'elemento description al corpo della pagina per renderlo visibile
 description.id = "description"; // aggiungo un id all'elemento description per assegnargli lo stile nel file css
 
@@ -20,98 +21,134 @@ searchButton.id = "searchButton"; // aggiungo un id all'elemento searchButton pe
 divInputCity.appendChild(inputCity); // aggiungo l'elemento inputCity come figlio dell'elemento divInputCity
 divInputCity.appendChild(searchButton); // aggiungo l'elemento searchButton come figlio dell'elemento divInputCity
 
+document.getElementById("inputCity"); // recupero l'elemento inputCity grazie all'id
+inputCity.addEventListener("input", () => {
+  // mi metto in ascolto di inputCity
+  let inputValue = inputCity.value; // creo una variabile alla quale assegno il valore dell'input
+
+  inputValue = inputValue.toLowerCase(); // sostituisco eventuali lettere maiuscole con lettere minuscole che mi farebbero fallire la chiamata
+  inputCity.value = inputValue; // riassegno la città con le lettere minuscole a inputCity.value
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  let cityName = "";
+  // fino a che il contenuto del DOM non è stato caricato non eseguo nulla
+  let cityName = ""; // inizializzo la valiabile cityName
 
   searchButton.addEventListener("click", () => {
-    cityName = inputCity.value;
-    const basicUrl = `https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`;
+    // aggiungo l'addEventListener sul click del bottone search
 
-    const oldTitleDescription = document.getElementById("titleDescription");
-    const oldDescriptionCity = document.getElementById("descriptionCity");
-    const oldTitleCategory = document.getElementById("titleCategory");
-    const oldCategories = document.getElementById("categories");
-    const oldTitleScoreCity = document.getElementById("titleScoreCity");
-    const oldScoreCity = document.getElementById("scoreCity");
+    cityName = inputCity.value; // assegno a cityName il valore inserito nell'input
+    const basicUrl = `https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`; // basicUrl per la chiamata usando la variabile cityName per rendere dinamica la ricerca della città
+
+    const oldTitleDescription = document.getElementById("titleDescription"); // recupero l'elemento titleDescription dall'id
+    const oldDescriptionCity = document.getElementById("descriptionCity"); // recupero l'elemento descriptionCity dall'id
+    const oldTitleCategory = document.getElementById("titleCategory"); // recupero l'elemento titleCategory dall'id
+    const oldCategories = document.getElementById("categories"); // recupero l'elemento categories dall'id
+    const oldTitleScoreCity = document.getElementById("titleScoreCity"); // recupero l'elemento titleScoreCity dall'id
+    const oldScoreCity = document.getElementById("scoreCity"); // recupero l'elemento scoreCity dall'id
 
     if (oldTitleDescription) {
-      oldTitleDescription.remove();
+      oldTitleDescription.remove(); // elimino l'elemento oldTitleDescription per eliminare i vecchi dati
     }
     if (oldDescriptionCity) {
-      oldDescriptionCity.remove();
+      oldDescriptionCity.remove(); // elimino l'elemento oldDescriptionCity per eliminare i vecchi dati
     }
     if (oldTitleCategory) {
-      oldTitleCategory.remove();
+      oldTitleCategory.remove(); // elimino l'elemento oldTitleCategory per eliminare i vecchi dati
     }
     if (oldCategories) {
-      oldCategories.remove();
+      oldCategories.remove(); // elimino l'elemento oldCategories per eliminare i vecchi dati
     }
     if (oldTitleScoreCity) {
-      oldTitleScoreCity.remove();
+      oldTitleScoreCity.remove(); // elimino l'elemento oldTitleScoreCity per eliminare i vecchi dati
     }
     if (oldScoreCity) {
-      oldScoreCity.remove();
+      oldScoreCity.remove(); // elimino l'elemento oldScoreCity per eliminare i vecchi dati
     }
     fetch(basicUrl, {
-      method: "GET",
+      // faccio il fetch della chiamata usando il basicUrl dichiarato in precedenza
+      method: "GET", // eseguo una GET
     })
-      .then((response) => response.json())
+      .then((response) => response.json()) // recupero la response
       .then((data) => {
-        let titleDescription = document.createElement("div");
-        document.body.appendChild(titleDescription);
-        titleDescription.textContent = "Description of the city: ";
-        titleDescription.id = "titleDescription";
-
-        let descriptionCity = document.createElement("div");
-        document.body.appendChild(descriptionCity);
         if (data.summary) {
+          // controllo se esiste data.summary per effettaure in seguito della logica
+
+          let divCityInfo = document.createElement("div"); // creo l'elemento divCityInfo per visualizzare il div che continee le informazioni dall'API
+          document.body.appendChild(divCityInfo); // aggiungo l'elemento divCityInfo al corpo della pagina per renderlo visibile
+          divCityInfo.id = "divCityInfo"; // aggiungo un id all'elemento divCityInfo per assegnargli lo stile nel file css
+
+          let titleDescription = document.createElement("div"); // creo l'elemento titleDescription per visualizzare il div con il titolo della descrizione
+          document.body.appendChild(titleDescription); // aggiungo l'elemento titleDescription al corpo della pagina per renderlo visibile
+          titleDescription.textContent = "Description of the city: "; // assegno un valore al titolo della descrizione
+          titleDescription.id = "titleDescription"; // aggiungo un id all'elemento titleDescription per assegnargli lo stile nel file css
+
+          let descriptionCity = document.createElement("div"); // creo l'elemento descriptionCity per visualizzare il div con il titolo della descrizione della città
+          document.body.appendChild(descriptionCity); // aggiungo l'elemento descriptionCity al corpo della pagina per renderlo visibile
+
           descriptionCity.textContent = data.summary.replace(
             /<\/?p>|<\/?b>/g,
             ""
-          );
+          ); // elimino gli elementi non necessari nella descrizione della città dopo la chiamata e dopo aver verificato che esiste l'elemento data.summary
+          descriptionCity.id = "descriptionCity"; // aggiungo un id all'elemento titleCategory per assegnargli lo stile nel file css
         }
-        descriptionCity.id = "descriptionCity";
-
-        let titleCategory = document.createElement("div");
-        document.body.appendChild(titleCategory);
-        titleCategory.textContent = "Categories: ";
-        titleCategory.id = "titleCategory";
-
-        let categories = document.createElement("div");
-        document.body.appendChild(categories);
-        categories.id = "categories";
-        document.getElementById("categories");
 
         if (data.categories) {
+          // controllo se esiste data.categories per effettaure in seguito della logica
+
+          let titleCategory = document.createElement("div"); // creo l'elemento titleDescription per visualizzare il div con il titolo delle categorie della città
+          document.body.appendChild(titleCategory); // aggiungo l'elemento titleCategory al corpo della pagina per renderlo visibile
+          titleCategory.textContent = "Categories: "; // assegno un valore al titolo delle categorie
+          titleCategory.id = "titleCategory"; // aggiungo un id all'elemento titleCategory per assegnargli lo stile nel file css
+
+          let categories = document.createElement("div"); // creo l'elemento categories per visualizzare il div con le categorie della città
+          document.body.appendChild(categories); // aggiungo l'elemento categories al corpo della pagina per renderlo visibile
+          categories.id = "categories"; // aggiungo un id all'elemento categories per assegnargli lo stile nel file css
+          document.getElementById("categories"); // aggiungo un id all'elemento categories per assegnargli lo stile nel file css
+
           data.categories.forEach((element, index) => {
-            const categoryName = element.name;
-            const categoryScore = element.score_out_of_10;
+            // ciclo ogni categoria recuperata dalla chiamata
+            const categoryName = element.name; // assegno il nome di ogni categoria a categoryName
+            const categoryScore = element.score_out_of_10; // assegno lo score di ogni categoria a categoryScore
 
             const category = document.createTextNode(
               `Name: ${categoryName}, Score: ${categoryScore}`
-            );
+            ); // creo un nodo testo con una stringa formata dalla concatenazione delle variabili categoryName e categoryScore
 
-            categories.appendChild(category);
+            categories.appendChild(category); // aggiungo l'elemento category come figlio dell'elemento categories
 
             if (index < data.categories.length - 1) {
-              categories.appendChild(document.createElement("br"));
+              categories.appendChild(document.createElement("br")); // mando a capo ogni categoria recuperata per una migliore visualizzazione
             }
           });
         }
 
-        let titleScoreCity = document.createElement("div");
-        document.body.appendChild(titleScoreCity);
-        titleScoreCity.textContent = "Score of the city: ";
-        titleScoreCity.id = "titleScoreCity";
-
-        let scoreCity = document.createElement("div");
-        document.body.appendChild(scoreCity);
         if (data.teleport_city_score) {
-          scoreCity.textContent = data.teleport_city_score;
+          // controllo se esiste data.teleport_city_score per effettaure in seguito della logica
+
+          let titleScoreCity = document.createElement("div"); // creo l'elemento titleScoreCity per visualizzare il div con il titolo dello score della città
+          document.body.appendChild(titleScoreCity); // aggiungo l'elemento titleScoreCity al corpo della pagina per renderlo visibile
+          titleScoreCity.textContent = "Score of the city: "; // assegno un valore al titolo dello score della città
+          titleScoreCity.id = "titleScoreCity"; // aggiungo un id all'elemento titleScoreCity per assegnargli lo stile nel file css
+
+          let scoreCity = document.createElement("div"); // creo l'elemento scoreCity per visualizzare il div con lo score della città
+          document.body.appendChild(scoreCity); // aggiungo l'elemento scoreCity al corpo della pagina per renderlo visibile
+
+          scoreCity.textContent = data.teleport_city_score; // assegno il valore dello score dalla chiamata all'elemento scoreCity
+          scoreCity.id = "scoreCity"; // aggiungo un id all'elemento scoreCity per assegnargli lo stile nel file css
         }
-        scoreCity.id = "scoreCity";
+
+        divCityInfo.appendChild(titleDescription); // aggiungo l'elemento titleDescription come figlio dell'elemento divCityInfo
+        divCityInfo.appendChild(descriptionCity); // aggiungo l'elemento descriptionCity come figlio dell'elemento divCityInfo
+        divCityInfo.appendChild(titleCategory); // aggiungo l'elemento titleCategory come figlio dell'elemento divCityInfo
+        divCityInfo.appendChild(categories); // aggiungo l'elemento categories come figlio dell'elemento divCityInfo
+        divCityInfo.appendChild(titleScoreCity); // aggiungo l'elemento titleScoreCity come figlio dell'elemento divCityInfo
+        divCityInfo.appendChild(scoreCity); // aggiungo l'elemento scoreCity come figlio dell'elemento divCityInfo
       })
       .catch((error) => {
+        alert(
+          "Error with the API request, city not found, try searching the name of the city in english"
+        ); // aggiungo un alert per avvisare degli errori con la chiamata API
         console.log("Error for API request", error);
       });
   });
